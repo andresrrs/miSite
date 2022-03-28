2240df3f5f904446535d12f1e7d19f7d97452dfe
andresrrs <c.andres1489@gmail.com>
2022-03-28 01:07:30 -0500

    se agrego la funcion de envio de informacion al formulario

---
+++ js/submitEmail.js
@@ -0,0 +1,10 @@
+const btnEnviar = document.getElementById("enviarEmail");
+btnEnviar.addEventListener('click',function(e){
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const  email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const message = document.getElementById('message').value;
+
+    window.location.href='mailto:c.andres1489@gmail.com?subject=EnvioDesdeFormulario&body=Nombre%3A%20${name}%0AEmail%3A%20${email}%0ATelefono%3A%20${phone}%0AMensaje%3A%20${message}';
+});
